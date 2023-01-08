a381025ef6140f3fe13b8249d955498a33826a3d
Johannes Mattmann <johannes.mattmann@gmail.com>
2023-01-08 20:45:23 +0100

    obstacleSpawner ist überarbeitet, ungetestet

--- godot/scripts/obstacleSpawner.gd
+++ godot/scripts/obstacleSpawner.gd
@@ -6,26 +6,48 @@ var rng = RandomNumberGenerator.new()
 export (float) var spawn_rate_min = .1
 export (float) var spawn_rate_max = 1.0
 
-export (NodePath) var types
+#export (NodePath) var types
 #onready var runner = get_node("/root/Main/Player/ObstacleSpawner")
 onready var obstacleContainer = get_node("/root/Main/RunnerLogic/ObstacleContainer")
+onready var obstacleList = get_node("/root/Main/ObstacleList")
 
 var count_down = 0.0
 var typeList
+var likelihoods = []
+
 
 func _ready():
 	rng.randomize()
+	var sum := 0.0
+	for elm in obstacleList.get_children():
+		likelihoods.append(elm.likelihood)
+		sum += elm.likelihood
+	for lh in likelihoods:
+		lh *= 1.0/sum
+
+func spawn_element():
+	var type = rng.randf()
+	var idx = 0
+	var lhSum = 0.0
+	while (lhSum < type):
+		lhSum += likelihoods[idx]
+		idx += 1
+	idx -= 1
+	
+	var typeHolder = obstacleList.get_child(idx)
+	var subIdx = rng.randi_range(0, typeHolder.scenes.size() - 1)
+	var spawnItem = typeHolder.scenes[subIdx]
+	var newElm = spawnItem.instance()
+	obstacleContainer.add_child(newElm)
+	newElm.global_translation = self.global_translation
+	newElm.rotation = rng.randf_range(-PI, PI)
+	newElm.global_translation += Vector3(rand_range(-10 , 10), 0, rand_range(-10 , 10))
+	
 	
 
-
-#func _process(delta):
-#	count_down -= delta
-#	if count_down < 0.0:
-#		var idx = rng.randi_range(0, types.size() - 1)
-#		var sceneList = types[idx].get_children()
-#		var subIdx = rng.randi_range(0, sceneList.size() - 1)
-#		var newElm = spawnableItems[idx].instance()
-#		obstacleContainer.add_child(newElm)
-#		newElm.global_translation = self.global_translation
-#		newElm.global_translation += Vector3(rand_range(-10 , 10), 0, rand_range(-10 , 10))
-#		count_down = rng.randf_range(spawn_rate_min, spawn_rate_max)
+func _process(delta):
+	count_down -= delta
+	if count_down < 0.0:
+		spawn_element()
+		
+		count_down = rng.randf_range(spawn_rate_min, spawn_rate_max)
